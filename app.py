c5f423ad650e4793d19c4c6ccd729a407909ebe6
Awais Khan <awaiskhn223@gmail.com>
2026-05-13 16:19:37 +0500

    Fixed model path

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import joblib
 import pandas as pd
+import os
 
 st.title("Fraud Detection System")
 st.write("Enter transaction details to check for fraud.")
 
-model = joblib.load("fraud_model.pkl")
+# Load model from the same folder as app.py
+model_path = os.path.join(os.path.dirname(_file_), "fraud_model.pkl")
+model = joblib.load(model_path)
 
 amount = st.number_input("Transaction Amount ($)", min_value=0)
 txn_type = st.selectbox("Transaction Type", ["Payment", "Transfer", "Cash Out"])
@@ -14,6 +17,6 @@ new_balance = st.number_input("New Balance ($)", min_value=0)
 
 if st.button("Check Transaction"):
     if amount > 100000 and new_balance == 0:
-        st.error("🚨 FRAUD DETECTED!")
+        st.error(" FRAUD DETECTED!")
     else:
-        st.success("✅ Transaction is Safe")
+        st.success(" Transaction is Safe")
